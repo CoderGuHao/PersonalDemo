7c4dff39abb675c16014bb89ed8c8542359915b4
CoderGuHao <guhao_814659370@qq.com>
2017-07-04 15:14:43 +0800

    FragmentDemo
    fragment+recyclerview结合应用

---
+++ FragmentDemo/app/src/main/java/com/example/gu_hao/fragmentdemo/News.java
@@ -0,0 +1,28 @@
+package com.example.gu_hao.fragmentdemo;
+
+/**
+ * Created by gh on 2017/7/4.
+ */
+public class News {
+
+    private  String title;
+
+    private  String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
